9ec8933baa67e5098092337f269a886fe78475be
Bishnu.np <146713265+Bishnu-alt@users.noreply.github.com>
2024-09-03 14:06:45 +0545

    Update number_plate.py
    to transfer img

--- car/number_plate.py
+++ car/number_plate.py
@@ -1,45 +1,50 @@
 import cv2
 import time
+import requests
 
 harcascade = "model/haarcascade_russian_plate_number.xml"
 
 cap = cv2.VideoCapture(0)
-
-cap.set(3, 640) # width
-cap.set(4, 480) # height
+cap.set(3, 640)  # width
+cap.set(4, 480)  # height
 
 min_area = 500
 count = 0
 last_save_time = time.time()
 save_interval = 5  # seconds
+server_url = 'http://<your_django_server_ip>:8000/upload/'
 
 while True:
     success, img = cap.read()
-
     plate_cascade = cv2.CascadeClassifier(harcascade)
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
     plates = plate_cascade.detectMultiScale(img_gray, 1.1, 4)
 
-    for (x,y,w,h) in plates:
+    for (x, y, w, h) in plates:
         area = w * h
-
         if area > min_area:
-            cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
-            cv2.putText(img, "Number Plate", (x,y-5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 0, 255), 2)
+            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.putText(img, "Number Plate", (x, y-5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 0, 255), 2)
 
             img_roi = img[y: y+h, x:x+w]
             cv2.imshow("ROI", img_roi)
 
-            # Save the image if the interval has passed
+            # Save and send the image if the interval has passed
             current_time = time.time()
             if current_time - last_save_time > save_interval:
-                cv2.imwrite("plates/scaned_img_" + str(count) + ".jpg", img_roi)
-                cv2.rectangle(img, (0,200), (640,300), (0,255,0), cv2.FILLED)
+                img_path = "plates/scaned_img_" + str(count) + ".jpg"
+                cv2.imwrite(img_path, img_roi)
+                cv2.rectangle(img, (0, 200), (640, 300), (0, 255, 0), cv2.FILLED)
                 cv2.putText(img, "Plate Saved", (150, 265), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 255), 2)
+                
+                # Send the image to the Django server
+                with open(img_path, 'rb') as img_file:
+                    response = requests.post(server_url, files={'file': img_file})
+                    print(response.json())
+                
                 last_save_time = current_time
                 count += 1
-    
+
     cv2.imshow("Result", img)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
